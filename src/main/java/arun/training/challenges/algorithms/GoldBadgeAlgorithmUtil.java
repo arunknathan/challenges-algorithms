4bc5ede787132a489644a3ce344f3329d6a11543
Arun K Nathan <arunknathan@gmail.com>
2018-10-18 21:56:37 +0530

    beautifulDays with tests

--- src/main/java/arun/training/challenges/algorithms/GoldBadgeAlgorithmUtil.java
+++ src/main/java/arun/training/challenges/algorithms/GoldBadgeAlgorithmUtil.java
@@ -5,7 +5,21 @@ import java.util.Arrays;
 public class GoldBadgeAlgorithmUtil {
 
 	public String angryProfessor(int k, int[] a) {
-		return Arrays.stream(a).filter(x -> x < 1).count() < k ? "YES" : "NO";
+		return Arrays.stream(a).parallel().filter(x -> x < 1).count() < k ? "YES" : "NO";
+	}
+
+	public int beautifulDays(int i, int j, int k) {
+		return (int) java.util.stream.IntStream.range(i, j).parallel().filter(x -> isBeautiful(k, x)).count();
+	}
+
+	private boolean isBeautiful(int k, int x) {
+		System.out.println("x=" + x + ";trimmedReverse="
+				+ Integer.parseInt(new StringBuilder("" + x).reverse().toString().replaceAll("^0+", "")) + ";k=" + k
+				+ ";result="
+				+ (Math.abs(x - Integer.parseInt(new StringBuilder("" + x).reverse().toString().replaceAll("^0+", "")))
+						% k));
+		return Math.abs(x - Integer.parseInt(new StringBuilder("" + x).reverse().toString().replaceAll("^0+", "")))
+				% k == 0;
 	}
 
 }
--- src/test/java/arun/training/challenges/algorithms/GoldBadgeAlgorithmUtilTest.java
+++ src/test/java/arun/training/challenges/algorithms/GoldBadgeAlgorithmUtilTest.java
@@ -12,4 +12,10 @@ public class GoldBadgeAlgorithmUtilTest {
 		Assert.assertEquals(util.angryProfessor(3, new int[] { -1, -3, 4, 2 }), "YES");
 		Assert.assertEquals(util.angryProfessor(2, new int[] { 0, -1, 2, 1 }), "NO");
 	}
+
+	@Test
+	public void testBeautifulDays() {
+		Assert.assertEquals(util.beautifulDays(20, 23, 6), 2);
+		Assert.assertEquals(util.beautifulDays(13, 45, 3), 32);
+	}
 }
